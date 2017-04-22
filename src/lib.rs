08efee068b8fbeebb669a83b576879382c4fc6bb
Jelte Fennema <github-tech@jeltef.nl>
2017-04-22 23:07:44 +0200

    Add some basic docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,33 @@
+//! It can be useful to not have to worry about missing keys in a map.
+//! If a key is requested that doesn't have a value a default value is simply returned.
+//! This is exactly what this library provides.
+//!
+
+//! A clear use case of this is when counting the unique elements in a list.
+//! Here you want to add one to the existing value in the map for that key.
+//! This is a problem for the first adition when there's no value for the key yet.
+//! With this library you can specify when creating the map that the default value should be zero.
+//!
+
+//! ```rust
+//! # use defaultmap::*;
+//! let nums = [1, 4, 3, 3, 4, 2, 4];
+//! let mut counts:  DefaultHashMap<i32, i32> = DefaultHashMap::new(0);
+//! // DefaultHashMap::default() is equivalent.
+//!
+//! for num in nums.into_iter() {
+//!     counts[*num] += 1;
+//! }
+//!
+//! println!("{:?}", counts);
+//! // DefaultHashMap { map: {1: 1, 3: 2, 2: 1, 4: 3}, default: 0 }
+//!
+//! # assert_eq!(1, counts[1]);
+//! # assert_eq!(1, counts[2]);
+//! # assert_eq!(2, counts[3]);
+//! # assert_eq!(3, counts[4]);
+//! ```
+
 #![recursion_limit="128"]
 #[macro_use]
 
@@ -129,5 +159,19 @@ mod tests {
         assert_eq!(*map.get(&999), 0);
         assert_eq!(map[999], 0);
         assert_eq!(map[&999], 0);
+    }
+
+    #[test]
+    fn counter() {
+        let nums = [1, 4, 3, 3, 4, 2, 4];
+        let mut counts: DefaultHashMap<i32, i32> = DefaultHashMap::default();
+        for num in nums.into_iter() {
+            counts[*num] += 1;
+        }
+
+        assert_eq!(1, counts[1]);
+        assert_eq!(1, counts[2]);
+        assert_eq!(2, counts[3]);
+        assert_eq!(3, counts[4]);
     }
 }
